457c9cc6faf0131492421147d630605eabf915eb
oblakov0372 <demaoblak1@gmail.com>
2023-09-24 14:20:32 +0300

    .

--- ProjectForCrypto/Data/Context/ProjectDBContext.cs
+++ ProjectForCrypto/Data/Context/ProjectDBContext.cs
@@ -1,11 +1,18 @@
 ﻿using Data.Entities;
 using Data.Entities.CryptoTracker;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Data.Context
 {
     public class ProjectDBContext:DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public ProjectDBContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public DbSet<UserEntity> Users { get; set; }
         public DbSet<TradeFutureEntity> Trades { get; set; }
         public DbSet<PortfolioEntity> Portfolios { get; set; }
@@ -15,7 +22,7 @@ namespace Data.Context
         public DbSet<TelegramUserEntity> TelegramUsers { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=OBLAKOV0372;Database=CryptoHelper;Trusted_Connection=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ProjectDBContext"));
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
